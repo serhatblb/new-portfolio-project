fdf7e062d160439bb3c270c6076cda259546dbdb
serhatblb <serhatblb23@gmail.com>
2024-01-20 23:11:25 +0300

    header kısmı bitirildi

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  
+  name:string ="Serhat"
+  surname: string= " Bülbül"
+  myJob: string = " WEB DEVELOPER"
 }
