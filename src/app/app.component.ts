b37f03f2a38407aa2916e952affd30c6bb825612
serhatblb <serhatblb23@gmail.com>
2024-01-20 21:19:38 +0300

    yeni projeye baslandı ve componentler olusturuldu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,30 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+//Components 
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { EducationComponent } from './education/education.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+      CommonModule, 
+      RouterOutlet,
+      NavbarComponent,
+      HeaderComponent,
+      AboutComponent,
+      BlogComponent,
+      EducationComponent,
+      FooterComponent,
+      ContactComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
