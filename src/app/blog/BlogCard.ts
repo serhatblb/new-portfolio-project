ef51846791abf057aaf7b8b8915f611613c627b4
serhatblb <serhatblb23@gmail.com>
2024-01-21 20:02:48 +0300

    blog componenti bitirildi, resimler icin tekrar bakılacak

---
+++ src/app/blog/BlogCard.ts
@@ -0,0 +1,36 @@
+// Blog Card
+export class BlogCard {
+  // Field
+  id: number;
+  header:string;
+  title: string;
+  text: string;
+  description: string;
+  // img: string;
+  author: string;
+  date: Date;
+
+  // constructor
+  constructor(
+    id: number,
+    header:string,
+    title: string,
+    text: string,
+    description: string,
+    // img: string,
+    author: string,
+    date: Date
+
+
+  ) {
+    this.id = id;
+    this.header = header;
+    this.title = title;
+    this.text = text;
+    this.description = description;
+    // this.img = img;
+    this.author = author;
+    this.date = date;
+  }
+
+}
